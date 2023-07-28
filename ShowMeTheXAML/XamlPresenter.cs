38aa245c21b14468849b01aba9fd3a8936d101f6
Steve Bilogan <steve.bilogan@gmail.com>
2023-07-28 15:49:43 -0400

    feat!: Move to WinUI, add Uno 5 support +semver:major

--- ShowMeTheXAML/XamlPresenter.cs
+++ ShowMeTheXAML/XamlPresenter.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 
 #if __UNO__
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 #else
 using System.Windows;
 using System.Windows.Controls;
